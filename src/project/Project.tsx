95f4a292dcae2a2c169d60fcc9c767d120aee647
Kseniya Smirnova <fanni@bk.ru>
2022-09-20 20:59:40 +0300

    css projects

--- src/project/Project.tsx
+++ src/project/Project.tsx
@@ -4,19 +4,21 @@ import style from './Project.module.css';
 type ProjectPropsType = {
     title: string
     discription: string
+    style: any
 }
 
 export const Project = (props: ProjectPropsType) => {
+
     return (
         <div className={style.project}>
-            <div className={style.projectImg}>
+            <div className={style.projectImg} style={props.style}>
                 <a href='' className={style.viewBtn}>view</a>
             </div>
-            <div className={style.projectTitle}>
-                <h3 >{props.title}</h3>
-            </div>
-            <span className={style.discription}>{props.discription}</span>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle} >{props.title}</h3>
 
+                <span className={style.discription}>{props.discription}</span>
+            </div>
         </div >
     )
 }
--- src/project/Projects.tsx
+++ src/project/Projects.tsx
@@ -3,19 +3,28 @@ import { Project } from './Project';
 import styleContainer from '../common/styles/Container.module.css';
 import style from './Projects.module.css';
 import { Title } from '../common/title/Title';
+import todoImage from './../assets/image/todo-list.webp'
+import socialImage from './../assets/image/social-network.webp'
 
 
 
 
 
 export const Projects = () => {
+   const socialNetwork = {
+       backgroundImage: `url(${socialImage})`,
+   }
+   const todoList = {
+       backgroundImage: `url(${todoImage})`,
+   }
+
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title text={'Projects'}/>
                 <div className={style.projects} >
-                    <Project title={'Social network'} discription={'description of your project. Brief and clea.'} />
-                    <Project title={'Todo list'} discription={'description of your project. Brief and clea'} />
+                    <Project style={socialNetwork} title={'Social network'} discription={'description of your project. Brief and clea.'} />
+                    <Project style={todoList} title={'Todo list'} discription={'description of your project. Brief and clea'} />
                 </div>
             </div>
         </div>
