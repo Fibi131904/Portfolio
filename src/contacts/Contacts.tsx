6c91c87ceb1213de10ec5c5d4fc2c228de24e776
Kseniya Smirnova <fanni@bk.ru>
2022-09-22 16:54:35 +0300

    css

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import Button from '../common/components/button/Button';
+import { Title } from '../common/title/Title';
 import style from './Contacts.module.scss';
 import { Form } from './Form';
 
@@ -7,12 +9,12 @@ import { Form } from './Form';
 export const Contacts = () => {
     return (
         <div className={style.contactBlock}>
-            <div className={style.contacts}>
-
-                <h2 className={style.title}> Contacts</h2>
-                <Form />
-                <button>Send</button>
-
+            <div >
+                <Title text={'Contacts'} />
+                <div className={style.contacts}>
+                    <Form />
+                    <Button>Send</Button>
+                </div>
             </div>
 
         </div>
--- src/distantWork/DistantWork.tsx
+++ src/distantWork/DistantWork.tsx
@@ -2,16 +2,19 @@
 import React from 'react';
 import style from './DistantWork.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import Button from '../common/components/button/Button';
+import { Title } from '../common/title/Title';
 
 
 export const DistantWork = () => {
     return (
         <div className={style.distantWorkBlock} >
 
-            <div className={`${styleContainer.container} ${style.contant}`}>
-                <h2 className={style.title}>Ready to work remotely</h2>
-                <button onClick={() => { }} >Write me</button>
+            <div className={style.contant}>
+                <Title text={'Ready to work remotely'} />
+
             </div>
+            <Button className={style.distantBtn} onClick={() => { }} >Write me</Button>
 
         </div >
     )
