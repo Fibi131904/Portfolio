b8d834890638f1b5e6aaa1a8163aef72e39562ad
Kseniya Smirnova <fanni@bk.ru>
2022-09-28 16:17:34 +0300

    css,icon

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import style from './Skills.module.scss';
 import { Skill } from './skill/Skill';
 import { Title } from '../common/title/Title';
+import { faCss3, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+
 
 
 
@@ -10,14 +14,15 @@ import { Title } from '../common/title/Title';
 
 
 export const Skills = () => {
+  
     return (
         <div className={style.skillsBlock}>
             <div className={style.skillsContainer} >
                 <Title text={'Skills'} />
                 <div className={style.skills}>
-                    <Skill title={'JS'} discription={'HTML5, CSS (SASS, SCSS, LESS) + responsive, JavaScript (ES6+), React.js + Redux, AntD, JQuery, Bootstrap, NPM, Gulp, Webpack, GitLab, GitHub'} />
-                    <Skill title={'CSS'} discription={'HTML, CSS + SCSS, Js + Reactjs + Redux, REST API, Git + GitHub, Typescript'} />
-                    <Skill title={'React'} discription={'HTML, CSS/SASS/PostCSS, БЭМ. Frontend: React/Redux (3 года), Vue'} />
+                    <Skill title={'JS'} discription={'HTML5, CSS (SASS, SCSS, LESS) + responsive, JavaScript (ES6+), React.js + Redux, AntD, JQuery, Bootstrap, NPM, Gulp, Webpack, GitLab, GitHub'}icon={faJs} />
+                    <Skill title={'CSS'} discription={'HTML, CSS + SCSS, Js + Reactjs + Redux, REST API, Git + GitHub, Typescript'} icon={faCss3}/>
+                    <Skill title={'React'} discription={'HTML, CSS/SASS/PostCSS, БЭМ. Frontend: React/Redux (3 года), Vue'}icon={faReact}/>
                 </div>
             </div>
         </div >
--- src/skills/skill/skill.tsx
+++ src/skills/skill/skill.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import style from './Skill.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+
+
+
+
 
 type SkillPropsType = {
     title: string
     discription: string
+ icon: any
 }
+
 
 export const Skill = (props: SkillPropsType) => {
     return (
-        <div className={style.skill}>
-            <div className={style.icon}></div>
+        <div className={style.skill} >
+            <div className={style.icon}>  <FontAwesomeIcon icon={props.icon} /> </div>
+          
             <h3>{props.title}</h3>
             <span className={style.discription}>{props.discription}</span>
         </div >
