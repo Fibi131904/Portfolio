502277962c30693f6546d778cbd059d95c8d7343
Kseniya Smirnova <fanni@bk.ru>
2022-10-07 13:11:50 +0300

    скилы, описание к ним. частично проекты без оформления

--- src/project/Projects.tsx
+++ src/project/Projects.tsx
@@ -4,6 +4,7 @@ import style from './Projects.module.scss';
 import { Title } from '../common/title/Title';
 import todoImage from './../assets/image/todo-list.webp'
 import socialImage from './../assets/image/social-network.webp'
+import counter2 from './../assets/image/counter2.png'
 
 
 
@@ -16,14 +17,19 @@ export const Projects = () => {
    const todoList = {
        backgroundImage: `url(${todoImage})`,
    }
+   const counter = {
+       backgroundImage: `url(${counter2})`,
+   }
 
     return (
         <div className={style.projectsBlock}>
             <div className={style.projectsContainer}>
                 <Title text={'Projects'}/>
                 <div className={style.projects} >
-                    <Project style={socialNetwork} title={'Social network'} discription={'description of your project. Brief and clea.'} />
-                    <Project style={todoList} title={'Todo list'} discription={'description of your project. Brief and clea'} />
+                    <Project style={socialNetwork} title={'Social network'} discription={'Technology stack: ReactJS, TypeScript, Redux, RestAPI, Axios, Redux-Thunk, React-Router-Dom, Redux-Form, Formik, Ant Design'} />
+                    <Project style={todoList} title={'Todo list'} discription={'Technology stack: ReactJS, TypeScript, Redux Toolkit, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing, Storybook'} />
+                    <Project style={counter} title={'Counter with settings in a separate block'} discription={'Technology stack: ReactJS, TypeScript, Redux, Unit testing, CSS'} />
+                    <Project style={todoList} title={'Counter with settings in one block'} discription={'Technology stack: ReactJS, TypeScript, Redux, CSS'} />
                 </div>
             </div>
         </div>
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,8 +3,7 @@ import style from './Skills.module.scss';
 import { Skill } from './skill/Skill';
 import { Title } from '../common/title/Title';
 import { faCss3, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { icon } from '@fortawesome/fontawesome-svg-core';
+
 
 
 
@@ -20,9 +19,22 @@ export const Skills = () => {
             <div className={style.skillsContainer} >
                 <Title text={'Skills'} />
                 <div className={style.skills}>
-                    <Skill title={'JS'} discription={'HTML5, CSS (SASS, SCSS, LESS) + responsive, JavaScript (ES6+), React.js + Redux, AntD, JQuery, Bootstrap, NPM, Gulp, Webpack, GitLab, GitHub'}icon={faJs} />
-                    <Skill title={'CSS'} discription={'HTML, CSS + SCSS, Js + Reactjs + Redux, REST API, Git + GitHub, Typescript'} icon={faCss3}/>
-                    <Skill title={'React'} discription={'HTML, CSS/SASS/PostCSS, БЭМ. Frontend: React/Redux (3 года), Vue'}icon={faReact}/>
+                    <Skill title={'React'} discription={'JavaScript-based UI development library.'}icon={faReact}/>
+                    <Skill title={'Redux'} discription={'JavaScript library for managing and centralizing application state.'}icon={faJs} />
+                    <Skill title={'TypeScript'} discription={'A syntactic superset of JavaScript which adds static typing.'} icon={faCss3}/>
+                    <Skill title={'JS'} discription={'The Programming Language for the Web that conforms to the ECMAScript specification'}icon={faJs} />
+                    <Skill title={'HTML'} discription={'Hypertext Markup Language for describing the contents and appearance of Web pages.'} icon={faCss3}/>
+                    <Skill title={'CSS'} discription={'Stylesheet language used to describe the presentation of a document written in HTML.'} icon={faCss3}/>
+                    <Skill title={'SASS'} discription={'CSS preprocessor, which adds special features such as variables, nested rules and mixins into regular CSS.'}icon={faReact}/>
+                    <Skill title={'Jest'} discription={'JavaScript library for creating, running, and structuring tests.'}icon={faReact}/>
+                    <Skill title={'Rest API'} discription={'Architectural style for an application program interface (API) that uses HTTP requests to access and use data.'}icon={faJs} />
+                    <Skill title={'Axios'} discription={'Promise-based library used with Node.js and browser to make asynchronous JavaScript HTTP requests.'} icon={faCss3}/>
+                    <Skill title={'Postman'} discription={'API platform for building and using APIs'}icon={faJs} />
+                    <Skill title={'Material UI'} discription={'Library that allows import and use different components to create a user interface in React applications.'} icon={faCss3}/>
+                    <Skill title={'Storybook'} discription={'Tool for building UI components and pages in isolation.'} icon={faCss3}/>
+                    <Skill title={'Git'} discription={'DevOps tool used for source code management.'}icon={faReact}/>
+                    <Skill title={'Formik'} discription={'Small group of React components and hooks for building forms in React and React Native.'}icon={faReact}/>
+               
                 </div>
             </div>
         </div >
