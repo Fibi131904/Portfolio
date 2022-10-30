928ba3ba983919995b187c48ab2638922cd0cb57
Kseniya Smirnova <fanni@bk.ru>
2022-10-30 20:47:14 +0300

    photo

--- src/project/Projects.tsx
+++ src/project/Projects.tsx
@@ -4,7 +4,8 @@ import style from './Projects.module.scss';
 import { Title } from '../common/title/Title';
 import todoImage from './../assets/image/todo-list.webp'
 import socialImage from './../assets/image/social-network.webp'
-import counter2 from './../assets/image/counter2.png'
+import counterRedux from './../assets/image/counter2.webp'
+import counter from './../assets/image/counter1.webp'
 
 
 
@@ -18,8 +19,11 @@ export const Projects = () => {
    const todoList = {
        backgroundImage: `url(${todoImage})`,
    }
-   const counter = {
-       backgroundImage: `url(${counter2})`,
+   const counterImage1 = {
+       backgroundImage: `url(${counterRedux})`,
+   }
+   const counterImage2 = {
+       backgroundImage: `url(${counter})`,
    }
 
     return (
@@ -31,8 +35,8 @@ export const Projects = () => {
                 <div className={style.projects} >
                     <Project style={socialNetwork} title={'Social network'} discription={'Technology stack: ReactJS, TypeScript, Redux, RestAPI, Axios, Redux-Thunk, React-Router-Dom, Redux-Form, Formik, Ant Design'} />
                     <Project style={todoList} title={'Todo list'} discription={'Technology stack: ReactJS, TypeScript, Redux Toolkit, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing, Storybook'} />
-                    <Project style={counter} title={'Counter with settings in a separate block'} discription={'Technology stack: ReactJS, TypeScript, Redux, Unit testing, CSS'} />
-                    <Project style={todoList} title={'Counter with settings in one block'} discription={'Technology stack: ReactJS, TypeScript, Redux, CSS'} />
+                    <Project style={counterImage1} title={'Counter with settings in a separate block'} discription={'Technology stack: ReactJS, TypeScript, Redux, MaterialUI, CSS'} />
+                    <Project style={counterImage2} title={'Counter with settings in one block'} discription={'Technology stack: ReactJS, TypeScript, CSS'} />
                 </div>
             </div>
         </div>
